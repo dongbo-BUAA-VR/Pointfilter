ee5b0ff29371b5cde9369f55850fdad367932911
dongbo zhang <1433988518@qq.com>
2020-09-22 10:17:32 +0800

    Add files via upload

--- eval.py
+++ eval.py
@@ -1,80 +1,42 @@
-import os
-import sys
-import torch
+import scipy.spatial as sp
 import numpy as np
-from Pointfilter_Network_Architecture import pointfilternet
-from Pointfilter_DataLoader import PointcloudPatchDataset
-from Pointfilter_Utils import parse_arguments
+import torch
+import os
+from Customer_Module.chamfer_distance.dist_chamfer import chamferDist
+nnd = chamferDist()
 
+def Eval_With_Charmfer_Distance():
+    print('Errors under Chamfer Distance')
+    for shape_id, shape_name in enumerate(shape_names):
+        gt_pts = np.load(os.path.join('./Dataset/Test', shape_name[:-6] + '.npy'))
+        pred_pts = np.load(os.path.join('./Dataset/Results', shape_name + '_pred_iter_2.npy'))
+        with torch.no_grad():
+            gt_pts_cuda = torch.from_numpy(np.expand_dims(gt_pts, axis=0)).cuda().float()
+            pred_pts_cuda = torch.from_numpy(np.expand_dims(pred_pts, axis=0)).cuda().float()
+            dist1, dist2 = nnd(pred_pts_cuda, gt_pts_cuda)
+            chamfer_errors = torch.mean(dist1, dim=1) + torch.mean(dist2, dim=1)
+            print('%12s  %.3f' % (models_name[shape_id], round(chamfer_errors.item() * 100000, 3)))
 
-def eval(opt):
+def Eval_With_Mean_Square_Error():
+    print('Errors under Mean Square Error')
+    for shape_id, shape_name in enumerate(shape_names):
+        gt_pts = np.load(os.path.join('./Dataset/Test', shape_name[:-6] + '.npy'))
+        gt_pts_tree = sp.cKDTree(gt_pts)
+        pred_pts = np.load(os.path.join('./Dataset/Results', shape_name + '_pred_iter_2.npy'))
+        pred_dist, _ = gt_pts_tree.query(pred_pts, 10)
+        print('%12s  %.3f' % (models_name[shape_id], round(pred_dist.mean() * 1000, 3)))
 
-    with open(os.path.join(opt.testset, 'test.txt'), 'r') as f:
+if __name__ == '__main__':
+
+    with open(os.path.join('./Dataset/Test', 'test.txt'), 'r') as f:
         shape_names = f.readlines()
     shape_names = [x.strip() for x in shape_names]
     shape_names = list(filter(None, shape_names))
 
-    if not os.path.exists(parameters.save_dir):
-        os.makedirs(parameters.save_dir)
+    models_name = ['Boxunion',
+                   'Cube',
+                   'Fandisk',
+                   'Tetrahedron']
 
-    for shape_id, shape_name in enumerate(shape_names):
-        print(shape_name)
-        original_noise_pts = np.load(os.path.join(opt.testset, shape_name + '.npy'))
-        np.save(os.path.join(opt.save_dir, shape_name + '_pred_iter_0.npy'), original_noise_pts.astype('float32'))
-
-        for eval_index in range(opt.eval_iter_nums):
-            print(eval_index)
-            test_dataset = PointcloudPatchDataset(
-                root=opt.save_dir,
-                shape_name=shape_name + '_pred_iter_' + str(eval_index),
-                patch_radius=opt.patch_radius,
-                train_state='evaluation')
-            test_dataloader = torch.utils.data.DataLoader(
-                test_dataset,
-                batch_size=opt.batchSize,
-                num_workers=int(opt.workers))
-
-            pointfilter_eval = pointfilternet().cuda()
-            model_filename = os.path.join(parameters.eval_dir, 'model_full_ae.pth')
-            checkpoint = torch.load(model_filename)
-            pointfilter_eval.load_state_dict(checkpoint['state_dict'])
-            pointfilter_eval.cuda()
-            pointfilter_eval.eval()
-
-            patch_radius = test_dataset.patch_radius_absolute
-            pred_pts = np.empty((0, 3), dtype='float32')
-            for batch_ind, data_tuple in enumerate(test_dataloader):
-
-                noise_patch, noise_inv, noise_disp = data_tuple
-                noise_patch = noise_patch.float().cuda()
-                noise_inv = noise_inv.float().cuda()
-
-                noise_patch = noise_patch.transpose(2, 1).contiguous()
-                predict = pointfilter_eval(noise_patch)
-                predict = predict.unsqueeze(2)
-                predict = torch.bmm(noise_inv, predict)
-
-                pred_pts = np.append(pred_pts,
-                                     np.squeeze(predict.data.cpu().numpy()) * patch_radius + noise_disp.numpy(),
-                                     axis=0)
-            np.save(os.path.join(opt.save_dir, shape_name + '_pred_iter_' + str(eval_index + 1) + '.npy'),
-                    pred_pts.astype('float32'))
-
-
-
-if __name__ == '__main__':
-
-    parameters = parse_arguments()
-    parameters.testset = './Dataset/Test'
-    parameters.eval_dir = './Summary/pre_train_model/'
-    parameters.batchSize = 64
-    parameters.workers = 4
-    parameters.save_dir = './Dataset/Results/'
-    parameters.eval_iter_nums = 5
-    parameters.patch_radius = 0.05
-    eval(parameters)
-
-
-
-
-
+    Eval_With_Charmfer_Distance()
+    Eval_With_Mean_Square_Error()
